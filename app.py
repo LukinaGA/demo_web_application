88bb1f28e44d77270e5448f14efc7cdcf69c4519
Galina Lukina <lukina.galina11@gmail.com>
2024-10-14 21:59:56 +0400

    check

--- app.py
+++ app.py
@@ -4,17 +4,18 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 hostName = "localhost"
 serverPort = 8080
 
+
 class MyServer(BaseHTTPRequestHandler):
     """Специальный класс, отвечающий за обработку входящих запросов от клиентов"""
 
     def do_GET(self):
-        """ Метод для обработки входящих GET-запросов"""
+        """Метод для обработки входящих GET-запросов"""
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
         # чтение файла с кодом страницы "Контакты"
-        with open(os.path.join(os.path.dirname(__file__), "pages", "contacts.html"), encoding='utf-8') as f:
+        with open(os.path.join(os.path.dirname(__file__), "pages", "contacts.html"), encoding="utf-8") as f:
             content = f.read()
         self.wfile.write(bytes(content, "utf-8"))
 
